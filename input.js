a7d14f9d833f5819196e5d33c3464cdf6caaf97a
Hugo Stankowich <hstankowich@gmail.com>
2024-05-07 14:40:07 -0300

    Primeiro teste do github após meses, adicionei apenas um contador ao projeto, sem saber nada além do básico dessas tres linguagens, ainda.

--- input.js
+++ input.js
@@ -1,36 +1,46 @@
-const mario = document.querySelector (".mario")
-const pipe = document.querySelector (".pipe")
-const clouds = document.querySelector (".clouds")
-const jumpSound = document.getElementById ("pulo");
-const collisionSound = document.getElementById ("morte");
-const backgroundMusic = document.getElementById ("track");
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
+const jumpSound = document.getElementById("pulo");
+const collisionSound = document.getElementById("morte");
+const backgroundMusic = document.getElementById("track");
 
 let meuPontos = 0;
+let counterInterval; 
 
 document.addEventListener("click", () => {
     backgroundMusic.play();
+    startCounter();
 });
-
-
 
 const jump = () => {
     jumpSound.play();
-    mario.classList.add("jump")
+    mario.classList.add("jump");
 
     setTimeout(() => {
-        mario.classList.remove("jump")
-        }, 500)   
-}	
+        mario.classList.remove("jump");
+    }, 500);
+};
+
+const startCounter = () => {
+    counterInterval = setInterval(() => {
+        meuPontos++;
+        document.getElementById("counter").innerText = meuPontos;
+    }, 100);
+};
+
+const stopCounter = () => {
+    clearInterval(counterInterval);
+};
+
+const gameOverMsg = document.getElementById("game-over");
 
 const loop = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "")
-    const cloudPosition = clouds.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    const cloudPosition = clouds.offsetLeft;
 
-        console.log(pipePosition)
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
-        
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
 
@@ -38,23 +48,20 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = "imagens/game-over.png";
-        mario.style.width = "75px"
-        mario.style.marginLeft = "50px"
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
 
         clouds.style.animation = "none";
-        clouds.style.left = `${cloudPosition}px`
+        clouds.style.left = `${cloudPosition}px`;
           
-
         clearInterval(loop);
 
         collisionSound.play();
         backgroundMusic.pause();
+        stopCounter();
 
+        gameOverMsg.style.display = "block";
     }
-
 }, 10);
 
-
-document.addEventListener("keydown", jump)
-
-
+document.addEventListener("keydown", jump);
